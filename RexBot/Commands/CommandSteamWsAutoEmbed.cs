2b2641a84da22f65650fb137bf6566eb46996ff5
Brant Martin <rexxar-tc@users.noreply.github.com>
2017-12-05 22:56:20 -0500

    Fix TypeLoad errors (thanks Equinox)

--- RexBot/Commands/CommandSteamWsAutoEmbed.cs
+++ RexBot/Commands/CommandSteamWsAutoEmbed.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DSharpPlus.Entities;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using DSharpPlus.Entities;
 
-namespace RexBot.Commands
-{
-    public class CommandSteamWsAutoEmbed : IChatCommand
-    {
-        public const string SteamIconUrl = "https://images.weserv.nl/?url=store.steampowered.com%2Ffavicon.ico";
+//namespace RexBot.Commands
+//{
+//    public class CommandSteamWsAutoEmbed : IChatCommand
+//    {
+//        public const string SteamIconUrl = "https://images.weserv.nl/?url=store.steampowered.com%2Ffavicon.ico";
 
-        public CommandAccess Access => CommandAccess.Public;
-        public string Command => "https://steamcommunity.com/sharedfiles/filedetails";
-        public string HelpText => "Automatically embeds workshop information after workshop links";
-        public DiscordEmbed HelpEmbed { get; }
+//        public CommandAccess Access => CommandAccess.Public;
+//        public string Command => "https://steamcommunity.com/sharedfiles/filedetails";
+//        public string HelpText => "Automatically embeds workshop information after workshop links";
+//        public DiscordEmbed HelpEmbed { get; }
 
-        public async Task<string> Handle(DiscordMessage message)
-        {
-            await CommandSteamWsEmbed.HandleInternal(message.Content, message, true, "syntax error");
-            return null;
-        }
-    }
-}
+//        public async Task<string> Handle(DiscordMessage message)
+//        {
+//            await CommandSteamWsEmbed.HandleInternal(message.Content, message, true, "syntax error");
+//            return null;
+//        }
+//    }
+//}
